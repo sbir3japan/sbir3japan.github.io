b5460d365471b67d042701344c24b0b908551fcc
Alessandro Baffa <alessandro.baffa@gmail.com>
2021-08-31 08:56:27 +0900

    Make script to create articles more generic

--- new-post.sh
+++ new-post.sh
@@ -2,38 +2,30 @@
 
 title () {
     echo ""
-    echo "Please, select the title of the article (if you want to change it later, you will have to change the name of the file under $1/$2"
+    #echo "Please, select the title of the article (if you want to change it later, you will have to change the name of the file under $1/$2"
+    echo "-------------------------------------------------------------------------------------------------------------------"
+    echo "Insert the title of your article (you can always change it later, but remember to change the name of the file too):"
+    echo "-------------------------------------------------------------------------------------------------------------------"
     read input3
-    hugo new "docs/$1/$2/$input3.md"
+    hugo new "$1/$input3.md"
 }
 
-dev_categories () {
-    echo ""
-    echo "Please, select the sub-category:"
-    categories2=("cordapp_development" "design" "getting_started" "network_operations", "node_operations", "performance", "samples")
-    select cat2 in "${categories2[@]}"; do
-        echo "You chose $1/$cat2"
-        title $1 $cat2
-    done
-}
-
+echo "--------------------------------------------"
 echo "Please, select the category of your article:"
-categories1=("developers" "FAQ" "release_note" "understanding_corda")
-select cat1 in "${categories1[@]}"; do
-    echo "You chose $cat1"
-    dev_categories $cat1
-    # case $cat1 in
-    #     "developers")
-            
-    #         ;;
-    #     "FAQ")
-    #          echo "You chose $cat1"
-	#     # optionally call a function or run some code here
-    #         ;;
-	# "Quit")
-	#     echo "User requested exit"
-	#     exit
-	#     ;;
-    #     *) echo "invalid option $REPLY";;
-    # esac
+echo "--------------------------------------------"
+select d in $(find content/jp/docs/ -maxdepth 1 ! -path content/jp/docs/images ! -path content/jp/docs/ -type d)
+do  
+    ls $d/*/ >/dev/null 2>&1;
+    if [ $? == 0 ]
+    then
+        echo "------------------------------------------------"
+        echo "Please, select the sub-category of your article:"
+        echo "------------------------------------------------"
+        select e in $(find $d -maxdepth 1 ! -path $d -type d)
+        do
+            title $e; exit;
+        done
+    fi
+    
+    title $d; exit;
 done
--- usage-docs/creating-article.md
+++ usage-docs/creating-article.md
@@ -21,9 +21,11 @@ The file and folder layout matches what you see in the URL bar of your browser.
 
 There are two ways to create an article:
 
-* copy another article
-* use Hugo (preferred):
+1. copy another article
+2. use Hugo (preferred)
+3. use the `new-post.sh` script
 
+### Use Hugo (preferred)
 From the root folder of the repository:
 
 ```shell
@@ -48,6 +50,30 @@ This is a new article.
 Update the `tags` and `description` with the ones of your choice.
 
 You are free to change also the title even after the file has been generated (remember to also change the `linkTitle`). 
+
+### Use the new-post.sh script
+You can use an automated script where you need to enter the `category/subcategory/title` and it will generate the article automatically.
+
+```bash
+$ ./new-post.sh 
+Please, select the category of your article:
+1) developers
+2) FAQ
+3) release_note
+4) understanding_corda
+#? 1
+You chose developers
+
+Please, select the sub-category:
+1) cordapp_development  3) getting_started      5) node_operations      7) samples
+2) design               4) network_operations   6) performance
+#? 2
+You chose developers/design
+
+Please, select the title of the article (if you want to change it later, you will have to change the name of the file under developers/design
+this-is-a-test
+/home/brain/workspace/sbir3japan.github.io/content/jp/docs/developers/design/this-is-a-test.md created
+```
 
 ## Everything is Markdown
 
